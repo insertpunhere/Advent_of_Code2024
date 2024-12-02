f8e452dd46a9ffeb8c76afcd1306933614e9a37f
pettyDeath <ejames1998@gmail.com>
2024-12-02 10:55:34 -0800

    Update advent_1_2024.py

--- advent_1_2024.py
+++ advent_1_2024.py
@@ -3,7 +3,7 @@ input = open('advent_2024_day1_input.txt')
 input_list = input.readlines()
 left_list = []
 right_list = []
-answer = 0
+distance = 0
 similarity_score = 0
 
 for i in input_list:
@@ -20,8 +20,8 @@ for i in range(len(input_list)):
         x = left_list[i] - right_list[i]
     else:
        x = right_list[i] - left_list[i]
-    answer += x
-print(answer)
+    distance += x
+print('The distance is:', distance)
 # ^^^ Part One answer
 # Part Two
 
@@ -30,5 +30,5 @@ for i in range(len(left_list)):
     if right_list.count(left_list[i]):
         right_match_count += right_list.count(left_list[i])
         similarity_score += left_list[i] * right_match_count
-print(similarity_score)
+print('The similarity score is:', similarity_score)
 
